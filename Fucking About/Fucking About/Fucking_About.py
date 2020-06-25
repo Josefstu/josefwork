9c7e5aac3221fccb980aae479bdbc2af9112028a
Josefstu <59778458+Josefstu@users.noreply.github.com>
2020-06-24 22:22:20 -0400

    Committed after completion of Syntax module

--- Fucking About/Fucking About/Fucking_About.py
+++ Fucking About/Fucking About/Fucking_About.py
@@ -1,3 +1,20 @@
+# =========================================================================This is for testing all gay shit. No intelligent shit allowed ===========================================================
+
+
+
+# Testing numbers plus text, with numbers assigned to variables
 numer1 = 7
 numer2 = 5
 print("Number 1 plus number two is", numer1 + numer2)
+
+# I am a retard who struggles with anything past basic addition
+print(8 ** 2)
+#modulo gives remainder, it's not just division, retard.
+print(29 % 5)
+
+# Concatenation
+word1 = "I "
+word2 = "choke on "
+word3 = "men's nuts"
+message = word1 + word2 + word3
+print(message)
