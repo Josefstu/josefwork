83696e84ccb873cebfeca5dda914b1ebc323c5c4
Josefstu <59778458+Josefstu@users.noreply.github.com>
2020-06-27 19:08:44 -0400

    New commit adding additional notes, as well as vulgur visual and text

--- Fucking About/Fucking About/Fucking_About.py
+++ Fucking About/Fucking About/Fucking_About.py
@@ -1,16 +1,27 @@
 # =========================================================================This is for testing all gay shit. No intelligent shit allowed ===========================================================
 
-
+print("""                   
+           .........../´¯/) 
+....................,/¯../ 
+.................../    / 
+............./´¯/'   '/´¯¯`·¸ 
+........../'/.../    /       /¨¯\ 
+........('(   ´   ´     ¯~/'   ') 
+.........\.................'...../ 
+..........''...\.......... _.·´ 
+............\..............( 
+..............\.............\...
+""")
 
 # Testing numbers plus text, with numbers assigned to variables
 numer1 = 7
 numer2 = 5
-print("Number 1 plus number two is", numer1 + numer2)
+# print("Number 1 plus number two is", numer1 + numer2)
 
 # I am a retard who struggles with anything past basic addition
-print(8 ** 2)
+# print(8 ** 2)
 #modulo gives remainder, it's not just division, retard.
-print(29 % 5)
+# print(29 % 5)
 
 # Concatenation
 word1 = "I "
@@ -18,3 +29,18 @@ word2 = "choke on "
 word3 = "men's nuts"
 message = word1 + word2 + word3
 print(message)
+
+#Separate lines with triple quotes
+print("""
+
+""")
+
+# Define Function (fuckface1)
+def fuckface1(liquid):
+    print("Golly gee I sure do want to suck up some " + liquid)
+    print(liquid + " is the most delectable liquid a man can consume")
+# Call the function by referencing the defined name of the function, don't forget to define the parameter (liquid) set at the beginning of the function
+liquid = "gamer girl spit"
+fuckface1(liquid)
+
+
