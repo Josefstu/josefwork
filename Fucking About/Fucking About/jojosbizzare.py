2463c4a639fabb403a09874cdbf43bfdc7d41a1a
James Anderson <james88@umd.edu>
2020-06-27 20:53:50 -0400

    Added a truly bizzare experience

---
+++ Fucking About/Fucking About/jojosbizzare.py
@@ -0,0 +1,8 @@
+while input != 'Y' or input != 'N':
+    response = input('Josef has a tiny cock. Agree? Y/N\n')
+    if response == 'Y' or response == 'y':
+        print('Teehee,carrot-cock\n')
+        break
+    if response == 'N' or response == 'n':
+        print("You're wrong. I win, you lose. Bye bye.\n")
+        break
